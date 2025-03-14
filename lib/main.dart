e67cdfea5c551b7caa8234c3723f37f32b865045
Lin Wenhao <linwenhao5@gmail.com>
2025-03-14 18:20:37 +0100

    style: The home page shows the list of note

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:note_lite/screens/NoteListScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  runApp(NoteLite());
+}
+class NoteLite extends StatelessWidget {
+  const NoteLite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Note Lite",
+        theme: ThemeData(
+          primaryColor: Colors.blue
+        ),
+        home: NoteListScreen()
+    );
+  }
 }
